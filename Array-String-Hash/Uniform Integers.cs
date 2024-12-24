fec96d2a494b2793fe4f5a65db3fc55791c8ad0c
Vitor nocce <139376568+NocceV@users.noreply.github.com>
2024-12-23 21:07:32 -0300

    Create  Uniform Integers.cs

---
+++ Array-String-Hash/Uniform Integers.cs
@@ -0,0 +1,27 @@
+class Solution {
+  
+  public int getUniformIntegerCountInInterval(long A, long B) {
+    
+       
+        int digitosMax = B.ToString().Length;
+        int result = 0;
+
+        
+        for (int i = 1; i <= digitosMax; i++){
+            for (int j = 1; j <= 9; j++){
+               
+                long uniformNumber = long.Parse(new string(j.ToString()[0], i));
+                // Verificar se está no intervalo
+                if (uniformNumber >= A && uniformNumber <= B){
+                    result++;
+                }
+                if (uniformNumber > B){
+                    break;
+                }
+            }
+        }
+
+        return result;
+  }
+  
+}
