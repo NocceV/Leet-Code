a6c16435e4ab426986ed22aa344d00754d4af87b
Vitor nocce <139376568+NocceV@users.noreply.github.com>
2025-02-01 15:44:30 -0300

    Update RemoveDuplicates.cs

--- Array-String-Hash/Array/RemoveDuplicates/RemoveDuplicates.cs
+++ Array-String-Hash/Array/RemoveDuplicates/RemoveDuplicates.cs
@@ -2,20 +2,35 @@ public class Solution {
     public int RemoveDuplicates(int[] nums) {
         
 
-        int counter = 0;
+        //can the array be null or with 1 value?
+        //does the array contain only positive numbers?
 
-        for(int i = 1;i<nums.Length;i++){
+        //Create a pointer = 0
+        //Create a for loop 
+        //Inside the for loop i will compare the pointer qith the iterator
+        //if the pointer is equal the iterator i just continue
+        //else i will add 1 in the pointer and give the same value that the iterator
+        //return the pointer
 
-            if(nums[i] != nums[counter]){
-                counter++;
-                nums[counter] = nums[i];
+        //Time O(n)
+        //Space O(1)
+
+        // [1,2,3,5,9,10,3,5,9,10,10]
+            
+        //pointer = 5
+        // i = 9
+
+        int pointer = 0;
+        for(int i = 0; i< nums.Length;i++){
+
+            if(nums[pointer] != nums[i]){
+                pointer++;
+                nums[pointer] = nums[i];
             }
         }
 
-        return counter+1;
+        return pointer+1;
     }
 }
-
-//
 
 
