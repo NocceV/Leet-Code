84214f196fb17172547bcfc4c92a4e1431405a48
Vitor nocce <139376568+NocceV@users.noreply.github.com>
2025-02-09 14:30:33 -0300

    Update FindMedianSortedArrays.cs

--- Array-String-Hash/Array/Find MedianSortedArrays/FindMedianSortedArrays.cs
+++ Array-String-Hash/Array/Find MedianSortedArrays/FindMedianSortedArrays.cs
@@ -1,56 +1,108 @@
 public class Solution {
    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
     {
-        if (nums1.Length == 0)
-        {
-            return media(nums2);
-        }
-        if (nums2.Length == 0)
-        {
-            return media(nums1);
+        //     if (nums1.Length == 0)
+        //     {
+        //         return media(nums2);
+        //     }
+        //     if (nums2.Length == 0)
+        //     {
+        //         return media(nums1);
+        //     }
+
+        //     int tamanho = nums1.Length + nums2.Length;
+        //     int[] array = new int[tamanho];
+            
+        //     int ponteiron1 = 0, ponteiron2 = 0, i = 0;
+
+        //     while (ponteiron1 < nums1.Length && ponteiron2 < nums2.Length)
+        //     {
+        //         if (nums1[ponteiron1] <= nums2[ponteiron2])
+        //         {
+        //             array[i++] = nums1[ponteiron1++];
+        //         }
+        //         else
+        //         {
+        //             array[i++] = nums2[ponteiron2++];
+        //         }
+        //     }
+
+        //     while (ponteiron1 < nums1.Length)
+        //     {
+        //         array[i++] = nums1[ponteiron1++];
+        //     }
+
+        //     while (ponteiron2 < nums2.Length)
+        //     {
+        //         array[i++] = nums2[ponteiron2++];
+        //     }
+
+        //    return media(array);
+
+        //Revisao
+
+        //Create a array (N+M+1), that will save the numbers
+        //Create the pointers a and b
+        //create the sizes, n,m and global
+        //Create a media function
+        //Add values in a while loop, that will end in (n+m+1)/2
+        //Calls the media function
+        //Time = o(n)
+        //Space = O(n)
+
+        //Create n and m values, that represents the lenght of the arrays
+        //Create variables that represents the n+m lengths and the halve value
+        //Create variables that will be pointers for each array
+        //Create a itarator variable count
+        //Create the last 2 values, prev and current, prev represents the last value that will serve if the total length is par
+        //Inicialize the while loop, that goes only the half
+            //IF ponteir1 is less than n and the value inside the array is lass tha value i nthe array2, we put current = nums1[ponteir1]
+            //Else current = nums2[pointer2]
+        //Return the media
+        //Time =  O(n)
+        //Space = O(1)
+
+        int n = nums1.Length;
+        int m = nums2.Length;
+        int totalLength = n + m;
+        int half = (totalLength) / 2; 
+
+        int ponteiro1 = 0; 
+        int ponteiro2 = 0;
+        int count = -1;
+        int current = 0;
+        int prev = 0;
+
+        while (count < half) { 
+            prev = current; 
+            if (ponteiro1 < n && (ponteiro2 >= m || nums1[ponteiro1] <= nums2[ponteiro2])) {
+                current = nums1[ponteiro1++];
+            } else {
+                current = nums2[ponteiro2++];
+            }
+            count++;
         }
 
-        int tamanho = nums1.Length + nums2.Length;
-        int[] array = new int[tamanho];
+        if (totalLength % 2 == 0) {
+            return (current + prev) / 2.0;
+        } 
+        return current;
+    }
+
+    // public double media(int[] array){
+
+    //     // int meio = array.Length/2;
+    //     // if(array.Length%2==0){
+    //     //     return (double)(array[meio]+array[meio-1])/2;
+    //     // }
         
-        int ponteiron1 = 0, ponteiron2 = 0, i = 0;
+    //     // return array[meio];
 
-        while (ponteiron1 < nums1.Length && ponteiron2 < nums2.Length)
-        {
-            if (nums1[ponteiron1] <= nums2[ponteiron2])
-            {
-                array[i++] = nums1[ponteiron1++];
-            }
-            else
-            {
-                array[i++] = nums2[ponteiron2++];
-            }
-        }
+    //     if(array.Length%2==0){
+    //         return (double)(array[array.Length-2]+array[array.Length-1])/2;
+    //     }
 
-        while (ponteiron1 < nums1.Length)
-        {
-            array[i++] = nums1[ponteiron1++];
-        }
+    //     return array.Length-1;
+    // }
 
-        while (ponteiron2 < nums2.Length)
-        {
-            array[i++] = nums2[ponteiron2++];
-        }
-
-       return media(array);
-    }
-
-    public double media(int[] array)
-    {
-        int tamanho = array.Length;
-        int meio = tamanho / 2;
-        if (tamanho % 2 == 0)
-        {
-            return (array[meio - 1] + array[meio]) / 2.0;
-        }
-        else
-        {
-            return array[meio];
-        }
-    }
 }
