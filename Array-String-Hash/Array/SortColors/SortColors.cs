9aa28c781fe17c4be2f305465184edf057d9a4d8
Vitor nocce <139376568+NocceV@users.noreply.github.com>
2025-02-14 15:11:12 -0300

    Update SortColors.cs

--- Array-String-Hash/Array/SortColors/SortColors.cs
+++ Array-String-Hash/Array/SortColors/SortColors.cs
@@ -1,38 +1,45 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        
-        QuickSort(0,nums.Length-1,nums);
+        //Just call Array.sort()
+        //Time = O(nlog n)
+        //Space = O(n)
+
+        // Array.Sort(nums);
+
+        //Implement the QuickSort algorithim
+        //Time = O(nlog n)
+        //Space = O(n)
+
+        QuickSort(nums,0,nums.Length-1);
     }
 
-    static void QuickSort(int start, int end, int[] nums){
-        
-        if (start < end){
-            int pivot = Partition(start,end,nums);
-            QuickSort(start,pivot-1,nums);  
-            QuickSort(pivot+1,end,nums); 
+    public void QuickSort(int[] nums,int left, int right){
+
+        if(left < right){
+            int pivot = Core(nums,left,right);
+            QuickSort(nums,left,pivot-1);
+            QuickSort(nums,pivot+1,right);
         }
-    }
+    }   
 
-    static int Partition(int start,int end,int[] nums)
-    {
-        int pivot = nums[end]; 
-        int i = start - 1; 
+    public int Core(int[] nums,int left,int right){
 
-        for (int j = start; j < end; j++)
-        {
-            if (nums[j] <= pivot)
-            {
-                i++;
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+
+        int pivot = nums[right];
+        int pointer = left-1;
+
+        int temp = 0;
+
+        for(int i = left;i<=right;i++){
+
+            if(nums[i] <= pivot){
+                pointer++;
+                temp = nums[pointer];
+                nums[pointer] = nums[i];
+                nums[i] = temp;
             }
         }
 
-        int temp1 = nums[i + 1];
-        nums[i + 1] = nums[end];
-        nums[end] = temp1;
-
-        return i + 1;
+        return pointer;
     }
 }
