1c39588d3b81491d2ebff522f2fa911efbfa6f6f
Vitor nocce <139376568+NocceV@users.noreply.github.com>
2025-02-18 12:54:28 -0300

    Update ReverseString.cs

--- Array-String-Hash/String/ReverseString/ReverseString.cs
+++ Array-String-Hash/String/ReverseString/ReverseString.cs
@@ -12,17 +12,39 @@ public class Solution {
         //trocar valor do ponteiro pelo temporario
         //atualizar ponteiro--
 
-        int ponteiro = s.Length-1;
-        int metade =  s.Length/2;
+        // int ponteiro = s.Length-1;
+        // int metade =  s.Length/2;
 
-        for(int i=0;i<metade;i++){
+        // for(int i=0;i<metade;i++){
 
-            char temp = s[i];
-            s[i] = s[ponteiro];
-            s[ponteiro] = temp;
-            ponteiro--;
-        }
-
+        //     char temp = s[i];
+        //     s[i] = s[ponteiro];
+        //     s[ponteiro] = temp;
+        //     ponteiro--;
+        // }
         
+        //Revisao
+        
+        //Create a pointer left = 0
+        //Create a pointer right = s.Length-1
+        //Start a while loop into left < right
+            //Inside the loop we will switch the caracter places using the position of the pointers and a 
+            //temporary variable
+        //Time = O(n)
+        //Space = O(1)
+
+        int left = 0;
+        int right = s.Length-1;
+
+        char temp;
+        while(left<right){
+            
+            temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
     }
 }
