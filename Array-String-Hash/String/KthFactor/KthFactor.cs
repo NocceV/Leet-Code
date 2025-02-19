0c512cfda03f919d19a043d0dab0ab430ca0c77d
Vitor nocce <139376568+NocceV@users.noreply.github.com>
2025-02-19 12:43:06 -0300

    Update KthFactor.cs

--- Array-String-Hash/String/KthFactor/KthFactor.cs
+++ Array-String-Hash/String/KthFactor/KthFactor.cs
@@ -13,31 +13,72 @@ public class Solution {
         //depois se o tamanho da lista for menor que k retorna -1
         //se nao retorno o numero na posicao de k-1
 
-        List<int> lista = new List<int>();
+        // List<int> lista = new List<int>();
 
-        int iterador = 1;
+        // int iterador = 1;
 
-        while(iterador <= n){
+        // while(iterador <= n){
 
-            if(n % iterador == 0){
-                lista.Add(iterador);
-                iterador++;
-            }
-            else{
-                iterador++;
-            }
+        //     if(n % iterador == 0){
+        //         lista.Add(iterador);
+        //         iterador++;
+        //     }
+        //     else{
+        //         iterador++;
+        //     }
 
-            if(lista.Count == k){
-                int tamanho = lista.Count;
-                return lista[tamanho-1];
-            }
-        } 
+        //     if(lista.Count == k){
+        //         int tamanho = lista.Count;
+        //         return lista[tamanho-1];
+        //     }
+        // } 
 
-        if(lista.Count < k){
+        // if(lista.Count < k){
+        //     return -1;
+        // }
+        // else{
+        //     return lista[k-1];
+        // }
+
+        //Revisao
+
+        //If k > n, return -1
+        //Create a kComparer to compare the value k
+        //Create a result variable to store the result value
+        //Create a iterator i = 1 to start a while loop
+        //Start a while loop with i <= n
+            //if n % i == 0
+                //We add the value of the i into the result variable
+                //if the kComparer++ == k, 
+                    //we just return the result
+            //else we just iterator++
+        //If we go at the i == n
+            //return -1
+        //Time = O(n)
+        //Space = O(1)
+
+        if(k>n){
             return -1;
         }
-        else{
-            return lista[k-1];
+
+        int result;
+        int kComparer = 0;
+        int iterator = 1;
+
+        while(iterator <= n){
+
+            if(n % iterator == 0){
+                result = iterator;
+                kComparer++;              
+                if(kComparer == k){
+                    return result;
+                }
+            }
+
+            iterator++;
         }
+
+        return -1;
+
     }
 }
