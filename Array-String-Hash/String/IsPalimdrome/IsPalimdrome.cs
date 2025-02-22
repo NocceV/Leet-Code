53447f6b36fd28a03aedc3b55a4ac57ccf78c6c5
Vitor nocce <139376568+NocceV@users.noreply.github.com>
2025-02-22 13:59:50 -0300

    Update IsPalimdrome.cs

--- Array-String-Hash/String/IsPalimdrome/IsPalimdrome.cs
+++ Array-String-Hash/String/IsPalimdrome/IsPalimdrome.cs
@@ -1,33 +1,90 @@
 public class Solution {
-    public bool IsPalindrome(int x) {
-        //oque é um palimdromo?
-        //conta o espaço?
-        //é so uma palavra?
-        //pode ter qualquer caractere?
-        //sao so numeros?
+    public bool IsPalindrome(string s) {
+        //eu tiro todos os espaços em branco?
+        //eu tiro as letaas maiusculas?
+        //a string pode vir com caracteres especiais?
+        //a string pode vir numeros?
 
-        //entao eu recebo um numero inteiro sendo negativo ou positivo
-        //e verifico se ele é um palimdromo
+        //entao eu recebo uma string, 
+        //coloco ela em letras minusculas
+        //tiro os espacos em branco
+        //tiro os caracteres especiais
+        //comparo ela para ver se é um palimdromo
 
-        //inicio uma variavel = x.Length-1
-        //inicio uma variavel booleana épalimdromo = true
-        //inicio um for ate metade do tamanho de x
-        //vou aumentando o for e diminuindo a variavel iniciada anteriormente
-        //caso o caractere nas posições seja diferente eu retorno false
-        //caso seja verdadeiro eu continuo ate o fim.
-        //retorno a variavel booleana
+        //boto tudo em lowercase
+        //faço virar um array que da split nos espaços em branco
+        //crio um ponteiro esquerda e um ponteiro direita
+        //vou comparando os ponteiros
+        //se eles forem iguais continuo
+        //se forem diferentes retorno false
+        //se a posicao dos ponteiros possuir um caractere especial eu dou o ponteiro ++ ou --
+        //caso va ate o final do repeidor quer dizer q é palindromo e eu retorno true
 
-        string str = x.ToString();
-        int ponteiro = str.Length-1;
-        int metade = str.Length/2;
-        bool epalimdromo = true;
-        for(int i = 0;i<=metade;i++){
-            if(str[i] != str[ponteiro]){
-                epalimdromo = false;
-                return epalimdromo;
-            }
-            ponteiro--;
+        // s= s.ToLower();
+        // bool IsPalindrome = true;
+
+        // int left = 0;
+        // int right = s.Length-1;
+
+        // while(left < right){
+
+        //     if(s[left] == ' ' || !char.IsLetterOrDigit(s[left])){
+        //         left++;
+        //     }
+        //     else if(s[right] == ' ' || !char.IsLetterOrDigit(s[right])){
+        //         right--;
+        //     }
+        //     else if(s[left] != s[right]){
+        //         IsPalindrome = false;
+        //         return IsPalindrome;
+        //     }
+        //     else{
+        //         right--;
+        //         left++;
+        //     }
+        // }
+
+        // return IsPalindrome;
+
+        //Revisão
+
+        //Check if the string is null or one caracter
+        //Transform all the phrase in lowercase
+        //Create the Left and Right pointers
+        //Create a "while" that left wants to be menor than right
+        //Compare the values in the string
+        //If in left or right is " " or special caracters we just skip with Left++ or Right-- depending where is the "error"
+        //If the values are equal e just continue Left++ and Right--
+        //Else we return false
+        //If the loop is over we return true because is a palindrome
+        //Time = O(n)
+        //Space = O(1)
+
+        if(s == null || s.Length == 1){
+            return true;
         }
-        return epalimdromo;
+
+        s = s.ToLower();
+
+        int left = 0;
+        int right = s.Length-1;
+
+        while(left<right){
+            if(s[left] == ' ' || !char.IsLetterOrDigit(s[left])){
+                left++;
+            }
+            else if(s[right] == ' ' || !char.IsLetterOrDigit(s[right])){
+                right--;
+            }
+            else if(s[right]==s[left]){
+                left++;
+                right--;
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
     }
 }
