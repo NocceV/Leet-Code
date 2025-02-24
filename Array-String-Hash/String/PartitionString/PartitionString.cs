c53aaf00fa334c343c40a6b810ad2aae9542ffa1
Vitor nocce <139376568+NocceV@users.noreply.github.com>
2025-02-24 13:01:37 -0300

    Update PartitionString.cs

--- Array-String-Hash/String/PartitionString/PartitionString.cs
+++ Array-String-Hash/String/PartitionString/PartitionString.cs
@@ -20,32 +20,106 @@ public class Solution {
         //se nao for igaul so adicionamos a string o valor atual
         //fazermos isso tudo novamente e mum outro for, porém de tras para frente para ter mais certeza
 
-        s.Trim();
+        // s.Trim();
 
-        int numMinCre = 1;
-        string atual = s[0].ToString();
-        for(int i =1;i<s.Length;i++){
-            if(atual.Contains(s[i])){
-                numMinCre++;
-                atual = s[i].ToString();
-            }
-            else{
-                atual+=s[i].ToString();
-            }
-        }
+        // int numMinCre = 1;
+        // string atual = s[0].ToString();
+        // for(int i =1;i<s.Length;i++){
+        //     if(atual.Contains(s[i])){
+        //         numMinCre++;
+        //         atual = s[i].ToString();
+        //     }
+        //     else{
+        //         atual+=s[i].ToString();
+        //     }
+        // }
          
-        int numMinDecre = 1;
-        atual = s[s.Length-1].ToString();
-        for(int i =s.Length-1;i>0;i--){
-            if(atual.Contains(s[i])){
-                numMinDecre++;
-                atual = s[i].ToString();
+        // int numMinDecre = 1;
+        // atual = s[s.Length-1].ToString();
+        // for(int i =s.Length-1;i>0;i--){
+        //     if(atual.Contains(s[i])){
+        //         numMinDecre++;
+        //         atual = s[i].ToString();
+        //     }
+        //     else{
+        //         atual+=s[i].ToString();
+        //     }
+        // }
+
+        // return Math.Min(numMinDecre,numMinCre);
+
+        //Revisao
+
+        //Create a string variable that will increment the values
+        //Create a int variable that will cont the incresing partitions
+        //Create a for loop that will interate all the string
+            //If the string variable contains the s[i]
+                //We cocrescente++
+                //Restart the string variable
+            //Else we just
+                //String variavble +=
+        //We do this again but decresing
+
+        //Time = O(n^2)
+        //Space = O(1)
+
+        // string temp = s[0].ToString();
+        // int incre = 0;
+
+        // for(int i = 0;i<s.Length;i++){
+
+        //     if(temp.Contains(s[i])){
+        //         temp=s[i].ToString();
+        //         incre++;
+        //     }
+        //     else{
+        //         temp+=s[i].ToString();
+        //     }
+        // }
+
+        // int decre = 0;
+
+        // for(int i = s.Length-1;i>=0;i--){
+
+        //     if(temp.Contains(s[i])){
+        //         temp=s[i].ToString();
+        //         decre++;
+        //     }
+        //     else{
+        //         temp+=s[i].ToString();
+        //     }
+        // }
+
+        // return Math.Min(incre,decre);;
+
+        // return incre;
+
+        //Create a hashTable to compare the values
+        //Create a count variable
+        //Start a for loop
+            //If the table contains the character
+                //We clear the table
+                //And count++
+            //else
+                //Table.Add(value)
+        //return count
+
+        //Time = O(n^2)
+        //Space = O(n)
+        //Se saiu melhor
+        HashSet<char> table = new HashSet<char>();
+
+        int count = 1;
+
+        for(int i=0;i<s.Length;i++){
+            if(table.Contains(s[i])){
+                table.Clear();
+                count++;
             }
-            else{
-                atual+=s[i].ToString();
-            }
+
+            table.Add(s[i]);
         }
 
-        return Math.Min(numMinDecre,numMinCre);
+        return count;
     }
 }
