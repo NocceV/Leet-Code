17f91ba0f1aa658a775ea78a8fc53141e7214e68
Vitor nocce <139376568+NocceV@users.noreply.github.com>
2025-02-26 13:01:31 -0300

    Update StrStr.cs

--- Array-String-Hash/String/StrStr/StrStr.cs
+++ Array-String-Hash/String/StrStr/StrStr.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
         
-         // Caso especial: needle é vazio
+        //  // Caso especial: needle é vazio
         if (string.IsNullOrEmpty(needle)) return 0;
 
         // Iterar pelo haystack até o ponto onde needle pode caber
@@ -15,5 +15,57 @@ public class Solution {
 
         // Se não encontrou correspondência
         return -1;
+
+
+        //Revisao
+
+        //Fix the strings or check if the strings are null or empty
+        //Create a pointerNeedle = 0
+        //Start a for loop i = 0
+            //If the pointerNeedle == haystack[i]
+                //pointerNeedle++
+            //else just continue
+            //if the pointerNeedle == needle.Length
+                //return 0 
+        //if the loop ends just return -1
+
+        if(string.IsNullOrEmpty(needle)){
+            return 0;
+        }
+
+        List<int> starts = new List<int>();
+
+        for(int i = 0;i<haystack.Length;i++){
+            if(haystack[i] == needle[0]){
+                starts.Add(i);
+            }
+        }
+
+        int startsPointer= 0;
+        int needlePointer = 0;
+        int indexNeedle = -1;
+
+        for(int i = 0;i<haystack.Length;i++){
+
+            if(haystack[i] == needle[needlePointer]){
+                if(needlePointer == 0){
+                    indexNeedle = i;
+                }
+                needlePointer++;
+                if(needlePointer==needle.Length){
+                    return indexNeedle;
+                }
+            }
+            else{
+                if(startsPointer >= starts.Count()){
+                    return -1;
+                }
+                i=starts[startsPointer++]-1;
+                needlePointer = 0;
+                indexNeedle = -1;
+            }
+
+        }
+
+        return -1;
     }
-}
